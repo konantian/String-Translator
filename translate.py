70151ac6108c18c92997121b008033d8ae18a666
Yuan Wang <wang17@ualberta.ca>
2021-06-11 11:07:53 -0600

    add post api

--- translate.py
+++ translate.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    lang : str,
+    text : str
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/translate")
+def translate(item : Item):
+    if item.lang == 'en':
+        return {"lang": "fr", "translation" : "Bonjour"}
